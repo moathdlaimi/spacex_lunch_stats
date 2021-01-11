02102a866c1c5a4220470edfece9979203fc6ec0
Moath <moath.dlaii@gmail.com>
2021-01-11 00:19:34 -0600

    created 2 types

--- schema.js
+++ schema.js
@@ -1,5 +1,26 @@
-const GraphQLObjectType = require('graphql');
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = require('graphql');
 
 //Lunch Type
 
-const LaunchType = new GraphQL
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    flightNumber: { type: GraphQLInt },
+    missionName: { type: GraphQLString },
+    launchYear: { type: GraphQLString },
+    launchDateLocal: { type: GraphQLString },
+    lunchSuccess: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
+  })
+});
+
+//Rocket Type
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocketID: { type: GraphQLString },
+    rocketName: { type: GraphQLString },
+    rocketType: { type: GraphQLString },
+  })
+});
