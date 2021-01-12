2cf6b1c754ab6d2074cf5e97ca93f857561bb0eb
Moath <moath.dlaii@gmail.com>
2021-01-12 02:14:24 -0600

    created client

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema.js');
-
+const cors = require('cors');
 const app = express();
+
+//Allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
